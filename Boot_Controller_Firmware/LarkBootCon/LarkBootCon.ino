efe361ff86cc020ac7f23e270647d129ee19bac2
hamslices <223341941+hamslices@users.noreply.github.com>
2025-10-25 18:59:12 -0400

    Created new 01.00.01-2 boiler plate code, added boot controller firmware for the arduino as well
    - created a new project for version 01.00.01-2 (reserved pins).
    - added boot controller firmware for the attiny boot controller (may need revised).

---
+++ Boot_Controller_Firmware/LarkBootCon/LarkBootCon.ino
@@ -0,0 +1,90 @@
+/*************************************************************************
+
+  ██╗  ██╗ █████╗ ███╗   ███╗███████╗██╗     ██╗ ██████╗███████╗███████╗
+  ██║  ██║██╔══██╗████╗ ████║██╔════╝██║     ██║██╔════╝██╔════╝██╔════╝
+  ███████║███████║██╔████╔██║███████╗██║     ██║██║     █████╗  ███████╗
+  ██╔══██║██╔══██║██║╚██╔╝██║╚════██║██║     ██║██║     ██╔══╝  ╚════██║
+  ██║  ██║██║  ██║██║ ╚═╝ ██║███████║███████╗██║╚██████╗███████╗███████║
+  ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚══════╝╚═╝ ╚═════╝╚══════╝╚══════╝
+                                                          HamSlices 2025
+**************************************************************************
+
+  Lark Power Management and Boot Controller Firmware    
+    11% of program space used, 2% of dynamic memory
+    (untested 8/16/2025)
+
+  - Debounce is done in hardware
+
+*************************************************************************/
+
+#define PowerUpWait 300
+
+/* Pin Definitions */
+#define PowerEnable 0
+#define nReset 1
+#define Boot0 2
+#define DebugLED 3
+#define PushBTN 4
+
+/* Global Vars */
+int BootFlag = 0;
+int PowerState = 0;
+
+void setup() {
+  /* IO Setup */
+  pinMode(PowerEnable, OUTPUT);  //Output
+  pinMode(nReset, OUTPUT);       //Output
+  pinMode(Boot0, OUTPUT);        //Output
+  pinMode(DebugLED, OUTPUT);     //Output
+  pinMode(PushBTN, INPUT);       //Input
+
+  /* Set IO States */
+  digitalWrite(PowerEnable, LOW);  //Power off
+  digitalWrite(nReset, LOW);       //Place in Reset
+  digitalWrite(Boot0, LOW);        //Boot From App
+  digitalWrite(DebugLED, HIGH);    //Turn LED OFF
+  delay(200);
+
+  /* Check Button Press */
+  if (digitalRead(PushBTN) == 0) {
+    BootFlag = 1;                 //set bootflag to 1
+    digitalWrite(DebugLED, LOW);  //Turn LED ON
+    while (digitalRead(PushBTN) == 0)
+      ;                            //Hold for Release
+    digitalWrite(DebugLED, HIGH);  //Turn LED OFF
+  }
+}
+
+void loop() {
+  /* Check BootFlag */
+  if (BootFlag) {
+    digitalWrite(Boot0, HIGH);        //Boot From Bootloader
+    digitalWrite(PowerEnable, HIGH);  //Power on
+    delay(PowerUpWait);               //Wait for power up
+    digitalWrite(nReset, LOW);        //Remove from Reset
+    BootFlag = 0;
+    PowerState = 1;
+  }
+
+  /* Check Button Press */
+  if (digitalRead(PushBTN) == 0) {
+    digitalWrite(Boot0, LOW);           //Boot From App
+    if (PowerState == 1) {              //Power State = ON
+      digitalWrite(nReset, LOW);        //Place in Reset
+      digitalWrite(PowerEnable, LOW);   //Power off
+      PowerState = 0;                   //State to Off
+    } else {                            //Power State = OFF
+      digitalWrite(PowerEnable, HIGH);  //Power on
+      delay(PowerUpWait);               //Wait for power up
+      digitalWrite(nReset, LOW);        //Remove from Reset
+      PowerState = 1;                   //State to On
+    }
+    digitalWrite(DebugLED, LOW);  //Turn LED ON
+    while (digitalRead(PushBTN) == 0)
+      ;                            //Hold for Release
+    digitalWrite(DebugLED, HIGH);  //Turn LED OFF
+  }
+}
+
+
+//Line 90
---
+++ LarkFW/Core/Inc/main.h
@@ -0,0 +1,139 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define VREF_Pin GPIO_PIN_4
+#define VREF_GPIO_Port GPIOA
+#define SPI_CLK_Pin GPIO_PIN_5
+#define SPI_CLK_GPIO_Port GPIOA
+#define SPI_DATA_Pin GPIO_PIN_7
+#define SPI_DATA_GPIO_Port GPIOA
+#define n_LAT_Pin GPIO_PIN_4
+#define n_LAT_GPIO_Port GPIOC
+#define HUP_Pin GPIO_PIN_5
+#define HUP_GPIO_Port GPIOC
+#define PAPER_Pin GPIO_PIN_0
+#define PAPER_GPIO_Port GPIOB
+#define n_OE_Pin GPIO_PIN_1
+#define n_OE_GPIO_Port GPIOB
+#define MARK_Pin GPIO_PIN_11
+#define MARK_GPIO_Port GPIOE
+#define H_TMP_Pin GPIO_PIN_12
+#define H_TMP_GPIO_Port GPIOE
+#define DIR_Pin GPIO_PIN_14
+#define DIR_GPIO_Port GPIOE
+#define STEP_Pin GPIO_PIN_15
+#define STEP_GPIO_Port GPIOE
+#define n_SLEEP_Pin GPIO_PIN_10
+#define n_SLEEP_GPIO_Port GPIOB
+#define BTN2_Pin GPIO_PIN_14
+#define BTN2_GPIO_Port GPIOB
+#define BTN1_Pin GPIO_PIN_15
+#define BTN1_GPIO_Port GPIOB
+#define n_STB6_Pin GPIO_PIN_8
+#define n_STB6_GPIO_Port GPIOD
+#define n_STB4_Pin GPIO_PIN_9
+#define n_STB4_GPIO_Port GPIOD
+#define n_STB5_Pin GPIO_PIN_10
+#define n_STB5_GPIO_Port GPIOD
+#define n_STB3_Pin GPIO_PIN_11
+#define n_STB3_GPIO_Port GPIOD
+#define n_STB2_Pin GPIO_PIN_12
+#define n_STB2_GPIO_Port GPIOD
+#define n_STB1_Pin GPIO_PIN_13
+#define n_STB1_GPIO_Port GPIOD
+#define PWM1_Pin GPIO_PIN_6
+#define PWM1_GPIO_Port GPIOC
+#define PWM2_Pin GPIO_PIN_7
+#define PWM2_GPIO_Port GPIOC
+#define PWM3_Pin GPIO_PIN_8
+#define PWM3_GPIO_Port GPIOC
+#define EXT_GPIO_A_Pin GPIO_PIN_9
+#define EXT_GPIO_A_GPIO_Port GPIOC
+#define EXT_GPIO_B_Pin GPIO_PIN_8
+#define EXT_GPIO_B_GPIO_Port GPIOA
+#define n_RST_Pin GPIO_PIN_10
+#define n_RST_GPIO_Port GPIOA
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_Pin GPIO_PIN_14
+#define SWCLK_GPIO_Port GPIOA
+#define n_EN_Pin GPIO_PIN_1
+#define n_EN_GPIO_Port GPIOD
+#define M_TMP_Pin GPIO_PIN_3
+#define M_TMP_GPIO_Port GPIOD
+#define MS1_Pin GPIO_PIN_5
+#define MS1_GPIO_Port GPIOD
+#define MS2_Pin GPIO_PIN_7
+#define MS2_GPIO_Port GPIOD
+#define SCL_Pin GPIO_PIN_6
+#define SCL_GPIO_Port GPIOB
+#define SDA_Pin GPIO_PIN_7
+#define SDA_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
